965fda996b29554816dc66d278b52abf06a01704
Arvin <45604781+NivraLeak@users.noreply.github.com>
2021-07-13 22:20:52 -0500

    La cagada

---
+++ src/main/java/com/lovecode/life/tastelist/TastelistServerSideSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.lovecode.life.tastelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastelistServerSideSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastelistServerSideSoftwareApplication.class, args);
+    }
+
+}
